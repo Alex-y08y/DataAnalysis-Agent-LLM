6b15df3aae2885db571c3b553a8ff64b5c91fc1b
Alex-y08y <13310968863@163.com>
2026-07-29 20:25:36 +0800

    Add frontend/src/api/knowledge.js

---
+++ frontend/src/api/knowledge.js
@@ -0,0 +1,28 @@
+import request from './request'
+
+export function getKnowledgeDocs(params) {
+  return request.get('/knowledge', { params })
+}
+
+export function uploadKnowledgeDoc(data, onProgress) {
+  return request.post('/knowledge/upload', data, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+    onUploadProgress: onProgress,
+  })
+}
+
+export function deleteKnowledgeDoc(id) {
+  return request.delete(`/knowledge/${id}`)
+}
+
+export function rebuildIndex(id) {
+  return request.post(`/knowledge/${id}/rebuild`)
+}
+
+export function searchKnowledge(params) {
+  return request.get('/knowledge/search', { params })
+}
+
+export function updateKnowledgeDoc(id, data) {
+  return request.put(`/knowledge/${id}`, data)
+}
