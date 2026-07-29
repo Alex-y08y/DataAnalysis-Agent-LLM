072e05f75bba79712a8a9c1c1b4b32f4ce75687e
Alex-y08y <13310968863@163.com>
2026-07-29 20:25:34 +0800

    Add frontend/src/api/datasource.js

---
+++ frontend/src/api/datasource.js
@@ -0,0 +1,33 @@
+import request from './request'
+
+export function getDataSources(params) {
+  return request.get('/datasources', { params })
+}
+
+export function getDataSource(id) {
+  return request.get(`/datasources/${id}`)
+}
+
+export function createDataSource(data) {
+  return request.post('/datasources', data)
+}
+
+export function updateDataSource(id, data) {
+  return request.put(`/datasources/${id}`, data)
+}
+
+export function deleteDataSource(id) {
+  return request.delete(`/datasources/${id}`)
+}
+
+export function testConnection(id) {
+  return request.post(`/datasources/${id}/test`)
+}
+
+export function getTables(id) {
+  return request.get(`/datasources/${id}/tables`)
+}
+
+export function getTableSchema(id, tableName) {
+  return request.get(`/datasources/${id}/tables/${tableName}/schema`)
+}
